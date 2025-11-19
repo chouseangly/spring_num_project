62e038c5fbbcac14f0254fb85ef811056c5ba2b0
Rex <195429265+Rex-Z-z@users.noreply.github.com>
2025-11-19 10:01:48 +0700

    Update Overview tab

--- src/main/java/com/example/spring_project_mid/repository/CommentRepository.java
+++ src/main/java/com/example/spring_project_mid/repository/CommentRepository.java
@@ -1,6 +1,11 @@
 package com.example.spring_project_mid.repository;
 
-
 import com.example.spring_project_mid.model.Comment;
+import com.example.spring_project_mid.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-public interface CommentRepository extends JpaRepository<Comment, Long> {}
+
+import java.util.List;
+
+public interface CommentRepository extends JpaRepository<Comment, Long> {
+    List<Comment> findByUserOrderByCreatedAtDesc(User user);
+}
