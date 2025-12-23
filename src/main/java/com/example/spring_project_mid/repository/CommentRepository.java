5a381488590bafccb3ae4a81394c12c1458c583e
Rex <195429265+Rex-Z-z@users.noreply.github.com>
2025-12-23 15:54:29 +0700

    fix(auth): fix where user still see post after suspend

--- src/main/java/com/example/spring_project_mid/repository/CommentRepository.java
+++ src/main/java/com/example/spring_project_mid/repository/CommentRepository.java
@@ -2,10 +2,16 @@ package com.example.spring_project_mid.repository;
 
 import com.example.spring_project_mid.model.Comment;
 import com.example.spring_project_mid.model.User;
+
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
+    @EntityGraph(attributePaths = {"user", "post"})
     List<Comment> findByUserOrderByCreatedAtDesc(User user);
+
+    @EntityGraph(attributePaths = {"user", "post"})
+    List<Comment> findByUserAndSuspendedFalseOrderByCreatedAtDesc(User user);
 }
